3be5d1c35be29c761647d5965517fedb24dc5887
thsvo <155262940+thsvo@users.noreply.github.com>
2025-03-08 02:34:01 +0600

    final updated

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-center mb-8">Voice to Text Converter</h1>
         <VoiceRecorder />
       </div>
+      <div>Made with ❤️ by <a href="https://www.facebook.com/thsvoo">Tariqul Islam </a></div>
     </main>
   )
 }
